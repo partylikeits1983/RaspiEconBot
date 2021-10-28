e6fbc21f34f5f28f2cf156b929ff4c641c7b2a61
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-10-28 22:24:27 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,387 @@
+import pandas as pd
+import yfinance as yf
+import yahoofinancials
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime
+from datetime import date
+
+import matplotlib.pyplot as plt
+import quandl as ql
+
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+import numpy as np
+import matplotlib.pyplot as plt  # To visualize
+import pandas as pd  # To read data
+from sklearn.linear_model import LinearRegression
+
+
+#####how many days the correlation is looking at
+today = date.today()
+d = datetime.timedelta(days=30)
+start = today - d
+
+def plot_tickers(tickers, start, end, interval, track, log_plot, normalize):
+    data_df = yf.download(tickers,
+                          start=start,
+                          end=end,
+                          interval=interval,
+                          progress=False)
+
+    # Drop any NaNs (e.g. when comparing SPY to 'BTC-USD')
+    data_df = data_df.dropna()
+
+    # normalize df
+    if normalize == True:
+        data_df = (data_df - data_df.mean()) / data_df.std()
+    else:
+        pass
+
+    # Plot tickers
+    ticker_list = tickers.split(' ')
+
+    for ticker in ticker_list:
+        if ticker == 'BTC-USD':  # put BTC on right axis
+            ax = data_df[track, ticker].plot(secondary_y=True, figsize=(14, 10), legend=True, logy=log_plot, grid=True)
+        else:
+            ax = data_df[track, ticker].plot(figsize=(14, 10), legend=True, logy=log_plot, grid=True)
+
+    ax.get_legend().set_bbox_to_anchor((1.3, 1))
+
+    return data_df
+
+
+def calc_correlation(data_df, track):
+    # Get correlation and sort by sum
+    sum_corr = data_df[track].corr().sum().sort_values(ascending=True).index.values
+
+    data_df[track][sum_corr].corr()
+
+    # Call the df with the list from summed correlation, sorted ascending.
+    plt.figure(figsize=(13, 8))
+    ax = sns.heatmap(data_df[track][sum_corr].corr(),
+                     annot=True,
+                     cmap="Blues")
+
+    bottom, top = ax.get_ylim()
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+
+
+#BTC-USD ETH-USD 
+tickers = 'EURUSD=X RUB=X CL=F GLD SPY'
+
+# Timeframe
+start = '{}'.format(start)
+end = '{}'.format(today)
+
+# Time interval: can be 1m, 1h, 1d
+interval = '1d'
+
+# key to track: 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'
+track = 'Close'
+
+# plot options
+log_plot = False
+normalize = True
+
+# plot trends
+data_df = plot_tickers(tickers, start, end, interval, track, log_plot, normalize)
+#plt.savefig('/home/ubuntu/Desktop/TelegramBot/charts/visualcorrelation30.jpeg', dpi=400, bbox_inches='tight')
+
+# calculate and plot correlations
+calc_correlation(data_df, track)
+plt.savefig('/charts/correlationmatrix30.jpeg', dpi=400, bbox_inches='tight')
+
+
+#### Yield Curves:
+
+yield_ = ql.get("USTREASURY/YIELD")
+today = yield_.iloc[-1,:]
+month_ago = yield_.iloc[-30,:]
+df = pd.concat([today, month_ago], axis=1)
+df.columns = ['Today', '1 Month ago']
+#plt.style.use('classic')
+
+
+df.plot(style={'Today': 'ro-', '1 month ago': 'bx--'}
+        ,title='US Treasury Yield Curve, %', figsize=(15,10));
+
+plt.savefig('/charts/yieldUS.jpeg', dpi=400, bbox_inches='tight')
+
+
+
+###### Russian yield curve
+
+dt = datetime.datetime.today()
+year = dt.year
+month = dt.month
+day = dt.day - 1
+
+d = datetime.date(year, month, day)
+wd = d.weekday()
+
+if wd == 6:
+    day = dt.day - 2
+
+
+#yield curve CBRF now
+url = 'http://www.cbr.ru/eng/hd_base/zcyc_params/zcyc/?UniDbQuery.Posted=True&UniDbQuery.To=%sF%sF%s' % (day, month, year)       
+
+# Connect to the URL
+response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+
+index = []
+rates = []
+x = 1
+for i in range(12):
+    X = float(soup.findAll('th',{"class":""})[x].decode_contents())
+    Y = float(soup.findAll('td',{"class":""})[x].decode_contents())
+    
+    x+=1
+    index.append(X)
+    rates.append(Y)
+    
+
+print(rates)
+print(url)
+
+
+#yield curve CBRF one month ago
+dt = datetime.datetime.today()
+year = dt.year
+month = dt.month - 1
+day = dt.day
+
+d = datetime.date(year, month, day)
+wd = d.weekday()
+
+if wd == 6:
+    day = dt.day - 2
+
+# one month ago link:
+url = 'http://www.cbr.ru/eng/hd_base/zcyc_params/zcyc/?UniDbQuery.Posted=True&UniDbQuery.To=27%2F09%2F2021'
+
+# Connect to the URL
+response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+
+index = []
+rates1 = []
+x = 1
+for i in range(12):
+    X = float(soup.findAll('th',{"class":""})[x].decode_contents())
+    Y = float(soup.findAll('td',{"class":""})[x].decode_contents())
+    
+    x+=1
+    index.append(X)
+    rates1.append(Y)
+    
+
+print(rates1)
+print(url)
+
+# Russian bonds have different maturity rates as compared to US Tbonds
+index = ["3M0","6MO","9M","1Y","2Y","3Y","5Y","7Y","10Y","15Y","20Y","30Y"]
+
+df = pd.DataFrame(list(zip(index, rates, rates1)),
+               columns =['Maturity', 'Today', '1 Month ago'])
+
+
+df.plot(style={'Today': 'ro-', '1 month ago': 'bx--'}
+        ,title='Russian Government Bond Yield Curve, %', figsize=(15,10));
+
+
+plt.savefig('/charts/yieldRU.jpeg', dpi=400, bbox_inches='tight')
+
+
+
+##############################
+## macro indicators & data
+
+
+# INTEREST RATES
+
+# Set the URL you want to webscrape from
+url = 'https://fred.stlouisfed.org/series/INTDSRUSM193N'
+
+# Connect to the URL
+response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+
+KeyRateUS = soup.findAll('span',{"class":"series-meta-observation-value"})[0].decode_contents()
+
+print(KeyRateUS)
+
+
+
+#m1 MONEY SUPPLY
+url = 'https://fred.stlouisfed.org/series/WM1NS'
+
+# Connect to the URL
+response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+
+
+M1moneyUS = soup.findAll('span',{"class":"series-meta-observation-value"})[0].decode_contents()
+
+print(M1moneyUS)
+
+
+#student debt 
+url = 'https://fred.stlouisfed.org/series/SLOAS'
+
+# Connect to the URL
+response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+
+StudentDebtUS = soup.findAll('span',{"class":"series-meta-observation-value"})[0].decode_contents()
+
+print(StudentDebtUS)
+
+
+
+############# Russian indicator
+
+#Key interest rate CBRF
+url = 'https://www.cbr.ru/hd_base/KeyRate/'
+
+# Connect to the URL
+response = requests.get(url)
+
+# Parse HTML and save to BeautifulSoup object¶
+soup = BeautifulSoup(response.text, "html.parser")
+
+KeyRateRU = soup.findAll('td',{"class":""})[1].decode_contents()
+
+print(KeyRateRU)
+
+##### GINI Coefficient
+
+df = pd.read_csv('https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=GINIALLRH&scale=left&cosd=1967-01-01&coed=2020-01-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Annual&fam=avg&fgst=lin&fgsnd=2020-01-01&line_index=1&transformation=lin&vintage_date=2021-10-27&revision_date=2021-10-27&nd=1967-01-01')
+
+df = df.set_index('DATE')
+
+df.plot(figsize=(15,10))
+
+plt.savefig('/charts/giniUS.jpeg', dpi=400, bbox_inches='tight')
+
+df.to_csv("giniUS.csv")
+
+df = pd.read_csv('giniUS.csv')
+
+df = pd.read_csv('https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=GINIALLRH&scale=left&cosd=1967-01-01&coed=2020-01-01&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Annual&fam=avg&fgst=lin&fgsnd=2020-01-01&line_index=1&transformation=lin')
+
+df.drop('DATE', axis='columns', inplace=True)
+
+df.to_csv("giniUS.csv")
+
+pd.read_csv('giniUS.csv')
+
+data = pd.read_csv('giniUS.csv')  # load data set
+X = data.iloc[:, 0].values.reshape(-1, 1)  # values converts it into a numpy array
+Y = data.iloc[:, 1].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
+linear_regressor = LinearRegression()  # create object for the class
+linear_regressor.fit(X, Y)  # perform linear regression
+Y_pred = linear_regressor.predict(X)  # make predictions
+
+plt.scatter(X, Y)
+plt.plot(X, Y_pred, color='red')
+plt.savefig('/charts/giniUSlinRegression.jpeg', dpi=400, bbox_inches='tight')
+
+
+
+
+
+#### Debt
+
+driver = webdriver.Chrome()
+driver.get('https://www.usdebtclock.org/world-debt-clock.html')
+
+# USA NATIONAL DEBT
+USND = driver.find_element_by_xpath('//span[@id="X2a5BWRG"]').text
+# USA GDP NOMINAL
+USGDP = driver.find_element_by_xpath('//span[@id="X4a79R9BW"]').text
+# USA PUBLIC DEBT TO GDP RATIO
+USDR = driver.find_element_by_xpath('//span[@id="X3a34729DW"]').text
+# USA EXTERNAL DEBT TO GDP
+USEDR = driver.find_element_by_xpath('//span[@id="X5a582BYTA"]').text
+
+# CHINA NATIONAL DEBT
+ZHND = driver.find_element_by_xpath('//span[@id="M2a0923KLS"]').text
+# CHINA GDP NOMINAL
+ZHGDP = driver.find_element_by_xpath('//span[@id="M4a951MKWX"]').text
+# CHINA PUBLIC DEBT TO GDP RATIO
+ZHDR = driver.find_element_by_xpath('//span[@id="M3a3182UUB"]').text
+# CHINA EXTERNAL DEBT TO GDP
+ZHEDR = driver.find_element_by_xpath('//span[@id="M5a371BYON"]').text
+
+# JAPAN NATIONAL DEBT
+JPND = driver.find_element_by_xpath('//span[@id="R2a9163KRX"]').text
+# JAPAN GDP NOMINAL
+JPGDP = driver.find_element_by_xpath('//span[@id="R4a189MKIK"]').text
+# JAPAN PUBLIC DEBT TO GDP RATIO
+JPDR = driver.find_element_by_xpath('//span[@id="R3a0202UKB"]').text
+# JAPAN EXTERNAL DEBT TO GDP
+JPEDR = driver.find_element_by_xpath('//span[@id="R5a672BYLM"]').text
+
+# GERMANY NATIONAL DEBT
+DEDR = driver.find_element_by_xpath('//span[@id="E2a8263KGD"]').text
+# GERMANY GDP NOMINAL
+DEGDP = driver.find_element_by_xpath('//span[@id="E4a59MKOP"]').text
+# GERMANY PUBLIC DEBT TO GDP RATIO
+DEDR = driver.find_element_by_xpath('//span[@id="E3a6302UMF"]').text
+# GERMANY EXTERNAL DEBT TO GDP
+DEEDR = driver.find_element_by_xpath('//span[@id="E5a172BYIB"]').text
+
+
+# UK NATIONAL DEBT
+UKND = driver.find_element_by_xpath('//span[@id="H2a6763MKJ"]').text
+# UK GDP NOMINAL
+UKGDP = driver.find_element_by_xpath('//span[@id="H4a17MKKN"]').text
+# UK PUBLIC DEBT TO GDP RATIO
+UKDR = driver.find_element_by_xpath('//span[@id="H3a3402ODV"]').text
+# UK EXTERNAL DEBT TO GDP
+UKEDR = driver.find_element_by_xpath('//span[@id="H5a382BYOL"]').text
+
+# RUSSIA NATIONAL DEBT
+RUND = driver.find_element_by_xpath('//span[@id="G2a5723KEC"]').text
+# RUSSIA GDP NOMINAL
+RUGDP = driver.find_element_by_xpath('//span[@id="G4a411MKMG"]').text
+# RUSSIA PUBLIC DEBT TO GDP RATIO
+RUDR = driver.find_element_by_xpath('//span[@id="G3a9582UWX"]').text
+# RUSSIA EXTERNAL DEBT TO GDP
+RUEDR = driver.find_element_by_xpath('//span[@id="G5a491BYOK"]').text
+
+
+print("USND = " + USND)
+print("USGDP = " + USGDP)
+print("USDR = " + USDR)
+print("USEDR = " + USEDR)
+
+print(RUND)
+print(DEDR)
+driver.close()
+
+a = str(USND)
+#b = "{}".format(a)
+file = open('USND.txt', 'w')
+file.write(a)
+file.close()
+
---
+++ main.py
@@ -0,0 +1,276 @@
+from datetime import date, time, tzinfo, timezone, datetime
+import datetime
+import pytz
+import schedule
+import time
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import datetime
+from datetime import date
+from yahoo_fin import stock_info as si
+import csv
+
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+TOKEN = 'add your telegram token here'
+
+today = date.today()
+
+bot = telegram.Bot(TOKEN)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+
+    keyboard = [['/update', '/matricies'],
+               ['/yieldcurves', '/info'],
+               ['/list'],]
+
+    reply_markup = telegram.ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    bot.sendMessage(update.message.chat_id, text='Hello! Welcome to the Financial Forecast and Price Prediction Telegram bot!'
+                              ' This bot gives daily price correlations and future price predictions of'
+                              ' Bitcoin, Ether, Monero, USD/EUR, USD/RUB, PYPL, TSLA, SP500, and the Russel 2000!', reply_markup=reply_markup)
+
+    update.message.reply_text('Type /update to see the latest correlation matricies and price predictions.'
+                              ' Type /list to see the list of analyzed assets.'
+                              ' Type /info for more info. All charts are updated daily.')
+
+    from datetime import datetime
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+    first_name = update.message.chat.first_name
+    last_name = update.message.chat.last_name
+    username = update.message.chat.username
+    fullname = "{} {}".format(first_name, last_name)
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    logfile = [dt_string, chat_id, fullname, username, 'update']
+
+    with open('telegrambotlog.csv', 'a', newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(logfile)
+
+    print("{} Name: {} {} Username: {} Chat ID: {} Function: Start". format(dt_string, first_name, last_name , username, chat_id))
+
+
+
+def list(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        'Commands: \n/BTC \n/ETH \n/UNI \n/GOLD \n/OIL  \n/SP500 \n/EUR \n/RUB'
+        )
+
+
+def update(update, context):
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+
+
+    USND1 = open('USND.txt', 'r').read()
+    USGDP = open('USGDP.txt', 'r').read()
+    USDR = open('USDR.txt', 'r').read()
+    USEDR = open('USEDR.txt', 'r').read()
+
+    US = 'US National Debt: %s /n US Nominal GDP: %s /n US Internal Debt to GDP Ratio %s /n US External Debt to GDP Ratio: %s' % (USND1, USGDP, USDR, USEDR)
+
+
+    chat_id = update.message.chat_id
+    dailyupdate = ("▪{}").format(US)
+    bot.send_message(chat_id, dailyupdate)
+
+    update.message.reply_text(
+        'Type /list to view specific assets'
+        )
+
+
+    from datetime import datetime
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+    first_name = update.message.chat.first_name
+    last_name = update.message.chat.last_name
+    username = update.message.chat.username
+    fullname = "{} {}".format(first_name, last_name)
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    logfile = [dt_string, chat_id, fullname, username, 'update']
+
+    with open('/home/ubuntu/Desktop/telegrambotlog.csv', 'a', newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(logfile)
+
+    print("{} Name: {} {} Username: {} Chat ID: {} Function: Update". format(dt_string, first_name, last_name , username, chat_id))
+
+
+
+def matricies(update, context):
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/correlationmatrix180.jpeg', 'rb')
+    caption = "180 day correlation matrix {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/correlationmatrix30.jpeg', 'rb')
+    caption = "30 day correlation matrix {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+
+def yieldcurve(update, context):
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/yield.jpeg', 'rb')
+    caption = "Yield Curve {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+
+def GOLD(update, context):
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/GOLDforcast.jpeg', 'rb')
+    caption = "Gold forcast chart for {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/GOLDtrend.jpeg', 'rb')
+    caption = "Gold performance {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/GOLDforcastwithlines.jpeg', 'rb')
+    caption = "Gold performance {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+    photo = open('/home/ubuntu/Desktop/TelegramBot/charts/GOLDml.jpeg', 'rb')
+    caption = "Actual vs Predicted price of ML model {}".format(today)
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id, photo, caption)
+
+    GOLD1 = open('/home/ubuntu/Desktop/TelegramBot/predictions/futurepriceGOLD.txt', 'r').read()
+    GOLD2 = open('/home/ubuntu/Desktop/TelegramBot/predictions/meanabsoluteerrorGOLD.txt', 'r').read()
+    GOLD3 = open('/home/ubuntu/Desktop/TelegramBot/predictions/buyprofitGOLD.txt', 'r').read()
+    GOLD4 = open('/home/ubuntu/Desktop/TelegramBot/predictions/sellprofitGOLD.txt', 'r').read()
+    GOLD5 = open('/home/ubuntu/Desktop/TelegramBot/predictions/totalprofitGOLD.txt', 'r').read()
+    GOLD6 = open('/home/ubuntu/Desktop/TelegramBot/predictions/profitpertradeGOLD.txt', 'r').read()
+
+
+    if type(GOLD1) or type(GOLD2) == int or float:
+        #calculating Δ%
+        ngold = float(GOLD1)
+        s = pd.Series([si.get_live_price("GC=F"), ngold])
+        s.pct_change()
+        normal_sum = s.pct_change()
+        normal_sum.at[1]
+        dvGOLD = str(round((normal_sum.at[1] * 100), 2))
+        #calculating % error
+        ngold2 = float(GOLD2)
+        e = (ngold2 / ngold) * 100
+        eGOLD = '%.2f' % e
+        GOLD = 'Predicted price of Gold in 7 days $%s   (Δ%s%%)\n Model Error: %s%% \n Total buy profit: %s\n Total sell profit: %s \n Total profit: %s \n Profit per trade: %s \n' % (GOLD1, dvGOLD, eGOLD, GOLD3, GOLD4, GOLD5, GOLD6)
+        chat_id = update.message.chat_id
+        bot.send_message(chat_id, GOLD)
+    else:
+        text = 'Yahoo Finance is missing data for this asset. Could not run prediction model at this time.'
+        chat_id = update.message.chat_id
+        bot.send_message(chat_id, text)
+
+
+
+
+def info(update: Update, context: CallbackContext) -> None:
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    update.message.reply_text(
+        'This bot uses deep learning LSTM models to analyze time series data of asset prices. All charts and predictions are updated daily.'
+        ' Type /update to get stock price correlations.')
+
+    update.message.reply_text(
+        'This bot was created by Alexander Lee and uses Tensorflow to analyze time series data. (Go to https://www.tensorflow.org to read more)'
+        )
+
+    update.message.reply_text(
+        'What does it mean when the bot sends "percent error" alongside the price prediction? This is the machine learning model\'s average prediction error when it was training on the historical price data. In machine learning this is called "mean absolute error". Read more about it here: https://en.wikipedia.org/wiki/Mean_absolute_error'
+        )
+
+    update.message.reply_text(
+        'Type /moreinfo for more information'
+    )
+
+    from datetime import datetime
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+    first_name = update.message.chat.first_name
+    last_name = update.message.chat.last_name
+    username = update.message.chat.username
+    fullname = "{} {}".format(first_name, last_name)
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    logfile = [dt_string, chat_id, fullname, username, 'update']
+
+    with open('/home/ubuntu/Desktop/telegrambotlog.csv', 'a', newline='') as myfile:
+        wr = csv.writer(myfile)
+        wr.writerow(logfile)
+
+    print("{} Name: {} {} Username: {} Chat ID: {} Function: More Info". format(dt_string, first_name, last_name , username, chat_id))
+
+
+def moreinfo(update: Update, context: CallbackContext) -> None:
+    context.bot.sendChatAction(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
+    update.message.reply_text(
+        'If you like this bot please consider helping keeping it up and running! My ETH address: 0xC2e647AD0a1dF0EC67dC26EB39f3fD57171e13Fe\n This bot consumes on average 30w per hour @ 0.087 cents per kWh ~ $3 a month.')
+
+    update.message.reply_text('Disclaimer: This bot is provided for informational '
+                              ' and entertainment purposes only. Any price prediction data generated by this bot does not constitute investment advice.'
+                              ' If you like this bot, please consider sharing it!')
+
+
+def button(update: Update, _: CallbackContext) -> None:
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    query.edit_message_text(text=f"Selected option: {query.data}")
+
+
+def main():
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(CommandHandler("list", list))
+    dispatcher.add_handler(CommandHandler("update", update))
+    dispatcher.add_handler(CommandHandler("matricies", matricies))
+    dispatcher.add_handler(CommandHandler("yieldcurve", yieldcurve))
+    dispatcher.add_handler(CommandHandler("info", info))
+    dispatcher.add_handler(CommandHandler("moreinfo", moreinfo))
+    dispatcher.add_handler(CommandHandler("GOLD", GOLD))
+
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
