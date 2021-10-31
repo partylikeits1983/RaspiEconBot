74653f38a0f94e702588f1be2d5dcab2c5c8dd33
partylikeits1983 <aleqvids@gmail.com>
2021-10-31 16:40:07 +0300

    updates

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-TOKEN = 'add your telegram token here'
+TOKEN = str(input("input token"))
 
 today = date.today()
 
