e4257f35eb9ff4a6c810b1158edc6f24970f1206
partylikeits1983 <aleqvids@gmail.com>
2021-10-31 17:25:06 +0300

    update

--- automate.py
+++ automate.py
@@ -1,12 +1,17 @@
+
 import os
 import time as t
 from datetime import datetime
 import time as t
+import datetime
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+print("started automation")
+
 def some_job():
     os.system("gnome-terminal -e 'bash -c \"./automatebot.sh; exec bash\"'")
+    print("updated stats at " + str(datetime.datetime.now()))
 
 scheduler = BlockingScheduler()
 scheduler.add_job(some_job, 'interval', hours=1)
---
+++ automatebot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd 
+cd /home/pi/Desktop/RaspiEconBot
+
+python3 data.py && exit
+
