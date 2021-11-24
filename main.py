24defa6dc58cdfb8b2d2c6727d1bfc7cf4194ff0
Alexander Lee <77119221+partylikeits1983@users.noreply.github.com>
2021-11-24 14:14:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import pytz
 import schedule
 import time
 
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+from datetime import datetime
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
